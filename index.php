457d47de9a3e4b615f7f5fc78537d7fb8c790c49
merri zibert <mzibert@cnm.edu>
2016-04-13 13:32:28 -0600

    Created basic about me site Added 3 images for site

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE>
+	<html lang ="en">
+	<head>
+		<meta charset = "utf8"/>
+		<title> About crazy me...</title>
+	</head>
+	<body>
+		<h1> Why am I crazy?</h1>
+		<img src="images/be-you.jpeg" alt="Inspirational message"/>
+		<h2> FAMILY</h2>
+		<img src="images/Addams.jpg" alt="Creepy Familly Photo"/>
+			<ul>
+				<li> 2 daughters (16 years apart)</li>
+				<li>1 AMAZING husband</li>
+				<li>1 BIG dog (half shepherd half wolf</li>
+			</ul>
+		<h2> FUN</h2>
+		<ul>
+			<li> I like heavy weights and soft landings</li>
+			<li> I love Ketchup but hate tomatoes</li>
+			<li> I love heights but am allergic to flying!</li>
+			<li>I'm here arent I?</li>
+		</ul>
+	</body>
+	<footer> I can neither confirm or deny the validity of these statements</footer>
+	</html>
